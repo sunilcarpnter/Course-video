3482f825403b775e4902d0efc0e0d463d9fe8086
sunilcarpnter <sunilcarpnter@gmail.com>
2025-11-12 14:23:00 +0530

    Update BOT_TOKEN in vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "33880368"))
 API_HASH = environ.get("API_HASH", "d4a75dd7620aabcd07a38e805179ac42")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8529241793:AAEczt_8P3G6gx7E_rV5zEzSvxf_jL3kytA")
 
 OWNER = int(environ.get("OWNER", "1785431143"))
 CREDIT = environ.get("CREDIT", "SONU")
@@ -18,5 +18,6 @@ AUTH_USER = os.environ.get('AUTH_USERS', '1785431143').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
 
 
